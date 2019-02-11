ab69c1df9fa34a7affff49291128a761669edd2a
Kai Chang <kaijchang@gmail.com>
2019-02-10 19:42:48 -0800

    save sources and results and cleanup program output

--- main.py
+++ main.py
@@ -24,18 +24,22 @@ while True:
         else:
             title = article.find('h2').text
 
-        print(title)
         titles.append(title)
 
     page += 1
 
+word_regex = r'((?:[A-Z][a-z]+ ?)+)'
+
 patterns = [
-    [r'Can We Guess ((?:[A-Z][a-z]+ )+)Based On ((?:[A-Z][a-z]+ ?)+)\?', True]
+    [r'Can We Guess {word_regex} Based On {word_regex}\?', True]
 ]
+
+sources = []
+results = []
 
 for title in titles:
     for pattern, flipped in patterns:
-        match = re.match(pattern, title)
+        match = re.match(pattern.format(word_regex=word_regex), title)
 
         if match:
             source = match.group(1)
@@ -43,6 +47,11 @@ for title in titles:
 
             if flipped:
                 source, result = result, source
+
+            sources.append(source)
+            results.append(result)
+
+            pattern = pattern.format(word_regex='_____').replace('\\', '')
 
             print('''--------
 Title: {title}
@@ -52,3 +61,13 @@ Pattern: {pattern}
 --------'''.format(**locals()))
 
             continue
+
+
+with open('titles.json', 'w') as titles_file:
+    json.dump(titles, titles_file, indent=4)
+
+with open('sources.json', 'w') as sources_file:
+    json.dump(sources, sources_file, indent=4)
+
+with open('results.json', 'w') as results_file:
+    json.dump(results, results_file, indent=4)
